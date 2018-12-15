edf51d53b0379931a2be15b628cba245910dd8b6
Nicolas Charlton <nicolas.charlton@yahoo.degit config --global user.name Nicolas>
2018-12-16 00:26:32 +0100

    updated composer

---
+++ src/Controller/Admin/AdminClipController.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nick
+ * Date: 15.12.2018
+ * Time: 23:51
+ */
+
+namespace App\Controller\Admin;
+
+
+class AdminClipController
+{
+
+}
---
+++ src/Form/Admin/AdminClipType.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Nick
+ * Date: 16.12.2018
+ * Time: 00:09
+ */
+
+namespace App\Form\Admin;
+
+
+class AdminClipType
+{
+
+}
