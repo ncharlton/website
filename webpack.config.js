e51aeb8ab39210349db149aaea2ed565ae00dd9e
Nicolas Charlton <nicolas.charlton@yahoo.degit config --global user.name Nicolas>
2019-01-31 23:00:56 +0100

    added encore

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+var Encore = require('@symfony/webpack-encore');
+
+Encore
+    // directory where compiled assets will be stored
+    .setOutputPath('public/build/')
+    // public path used by the web server to access the output path
+    .setPublicPath('/build')
+
+    .addEntry('app', './assets/js/app.js')
+    //.addEntry('page1', './assets/js/page1.js')
+    //.addEntry('page2', './assets/js/page2.js')
+
+    .enableSingleRuntimeChunk()
+
+    /*
+     * FEATURE CONFIG
+     * https://symfony.com/doc/current/frontend.html#adding-more-features
+     */
+    .cleanupOutputBeforeBuild()
+    .enableBuildNotifications()
+    .enableSourceMaps(!Encore.isProduction())
+    .enableVersioning(Encore.isProduction())
+
+
+    .enableSassLoader()
+
+    .enableVueLoader()
+;
+
+module.exports = Encore.getWebpackConfig();
