1dadcd7d14a86619c5ec4f7f29751451116e9581
Nicolas Charlton <nicolas.charlton@yahoo.degit config --global user.name Nicolas>
2019-02-03 03:20:21 +0100

    added admin template

--- assets/js/admin.js
+++ assets/js/admin.js
@@ -1,1 +1,6 @@
+import $ from 'jquery';
+
+require('bootstrap/js/src')
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.min');
 require('../css/admin.scss');
